0726ec70f86ab53fb9deabd21cb68e1c682057e7
woorockets <woorockets@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-23 13:19:21 +0000

    Release version 2.4.6
    git-svn-id: https://plugins.svn.wordpress.org/wr-pagebuilder/trunk@1074077 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- core/helper/shortcode.php
+++ core/helper/shortcode.php
@@ -676,7 +676,7 @@ if ( ! class_exists( 'WR_Pb_Helper_Shortcode' ) ) {
 		 *
 		 * @return string $content
 		 */
-		static function remove_autop( $content, $autop = true ) {
+		static function remove_autop( $content) {
 			$shortcode_tags = array();
 			$tagregexp      = join( '|', array_map( 'preg_quote', $shortcode_tags ) );
 
@@ -708,14 +708,16 @@ if ( ! class_exists( 'WR_Pb_Helper_Shortcode' ) ) {
 				$content = str_replace( '[', '&#91;', $content );
 				$content = str_replace( '&#91;wr_', '[wr_', $content );
 				$content = str_replace( '&#91;/wr_', '[/wr_', $content );
-				$content = do_shortcode( $autop ? wpautop( $content ) : $content );
-			} else {
-				$content = do_shortcode( $autop ? wpautop( $content ) : $content );
 			}
+
+			$content = do_shortcode( $content );
 
 			// remove empty p tag which wrap <div>
 			$content = preg_replace( '/<p>(<!--[^>]*-->)*\n*(<div)/s', '$2', $content );
 			$content = preg_replace( '/(<\/div>)(.*)\n*<\/p>/s', '$1$2', $content );
+
+			// Replace each &#038; with &
+			$content = str_replace( '&#0f38;', '&', $content );
 
 			return balanceTags( $content );
 		}
